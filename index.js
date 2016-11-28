24115f78bc5e64d6b2c6904babfd77234210870e
Justin Thomas <jthomas0209@gmail.com>
2016-11-28 10:56:22 -0500

    done

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+const title = React.createElement('h1', {}, 'My first React Code');
+const paragraph = React.createElement('p', {}, 'My first paragraph in React');
+
 const list =
   React.createElement('div', {},
     React.createElement('h1', {}, 'My favorite ice cream flavors'),
     React.createElement('ul', {},
       [
-        React.createElement('li', { class: 'brown' }, 'Chocolate'),
-        React.createElement('li', { class: 'white' }, 'Vanilla'),
-        React.createElement('li', { class: 'yellow' }, 'Banana')
+        React.createElement('li', { className: 'brown' }, 'Chocolate'),
+        React.createElement('li', { className: 'white' }, 'Vanilla'),
+        React.createElement('li', { className: 'yellow' }, 'Banana')
       ]
     ));
 
-ReactDOM.render(list, document.getElementById('main'));
+const container = React.createElement('div', {}, [title,paragraph, list]);
+
+ReactDOM.render(container, document.getElementById('main'));
+//ReactDOM.render(list, document.getElementById('main'));
